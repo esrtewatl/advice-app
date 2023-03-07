998d57f256e6632741c342b642e167d107b5b582
Erica Stewart <114890276+esrtewatl@users.noreply.github.com>
2023-03-06 22:17:42 -0500

    search

--- src/App.js
+++ src/App.js
@@ -2,82 +2,70 @@ import { useState, useEffect } from "react";
 import "./App.css"
 
 function useDadJoke() {
-  const [text, setText] = useState({ id: "", joke: "" });
+  const [text, setText] = useState({ id: "", advice: "" });
+  
 
-  const fetchJokes = async (searchTerm) => {
-    const url = searchTerm
-      ? `https://icanhazdadjoke.com/search?term=${searchTerm}`
-      : "https://icanhazdadjoke.com/";
-  
-      const res = await fetch(url, {
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          "User-Agent": "Mozilla/5.0",
-        },
-      });
-      
+  const fetchDadJoke = async () => {
+    const res = await fetch("https://icanhazdadjoke.com/", {
+      headers: {
+        Accept: "application/json",
+      },
+    });
     const data = await res.json();
-  
-    if (searchTerm) {
-      setText({ id: "", joke: data.results[0]?.joke || "No jokes found" });
-    } else if (data && data.length > 0) { // add a check for data
-      const randomJoke = data[Math.floor(Math.random() * data.length)];
-      setText({ id: randomJoke.id, joke: randomJoke.joke });
-    }
+    console.log(data);
+    setText({ id: data.id, advice: data.joke });
   };
-  
 
   useEffect(() => {
-    fetchJokes();
+    fetchDadJoke();
   }, []);
 
   const handleClick = () => {
-    fetchJokes();
+    fetchDadJoke();
   };
 
-  const handleSearch = (event) => {
-    const searchTerm = event.target.value;
-    fetchJokes(searchTerm);
-  };
-
-  return { text, handleClick, handleSearch };
+  return { text, handleClick };
 }
 
-
 function App() {
-  const { text, handleClick, handleSearch } = useDadJoke();
+  const { text, handleClick } = useDadJoke();
+  
 
   return (
-    <div>
-      <div className="searchButton-container">
+<div>
+
         <input
           type="text"
           id="search"
           placeholder="Search Jokes"
           className="searchBar"
-          onChange={handleSearch}
+          
         />
-
+<div className="searchButton-container">
         <button className="searchButton"></button>
         <button className="searchButton"></button>
         <button className="searchButton"></button>
         <button className="searchButton"></button>
       </div>
+    <div className="container">
 
-      <div className="container">
-        <h1>Dad Joke #{text.id}</h1>
-        <p>{text.joke}</p>
 
-        <div>
-          <button onClick={handleClick}>
-            <img src="./dice.png" alt="" />
-          </button>
-        </div>
+      <h1>Dad Joke #{text.id}</h1>
+      <p>{text.advice}</p>
+
+      <picture>
+        <source media="(min-width: 768px)" srcSet="" />
+        <img src="" alt="" />
+      </picture>
+
+      <div>
+        <button onClick={handleClick}>
+          <img src="./dice.png" alt="" />
+        </button>
       </div>
+    </div>
     </div>
   );
 }
-
 
 export default App;
