a5b5c8d773f662f83caf8934a62c029d1d51fcb7
Erica Stewart <114890276+esrtewatl@users.noreply.github.com>
2023-03-07 10:40:00 -0500

    search and random

--- src/App.js
+++ src/App.js
@@ -1,69 +1,78 @@
 import { useState, useEffect } from "react";
-import "./App.css"
+import "./App.css";
 
 function useDadJoke() {
   const [text, setText] = useState({ id: "", advice: "" });
-  
 
-  const fetchDadJoke = async () => {
-    const res = await fetch("https://icanhazdadjoke.com/", {
+  const fetchDadJoke = async (query) => {
+    const url = `https://icanhazdadjoke.com/search?term=${query}`;
+    const res = await fetch(url, {
       headers: {
         Accept: "application/json",
       },
     });
     const data = await res.json();
     console.log(data);
+    setText({ id: data.results[0].id, advice: data.results[0].joke });
+  };
+
+  const fetchRandomDadJoke = async () => {
+    const res = await fetch("https://icanhazdadjoke.com/", {
+      headers: {
+        Accept: "application/json",
+      },
+    });
+    const data = await res.json();
     setText({ id: data.id, advice: data.joke });
   };
 
   useEffect(() => {
-    fetchDadJoke();
+    fetchRandomDadJoke();
   }, []);
 
   const handleClick = () => {
-    fetchDadJoke();
+    const query = document.getElementById("search").value;
+    fetchDadJoke(query);
   };
 
-  return { text, handleClick };
+  return { text, handleClick, fetchRandomDadJoke };
 }
 
 function App() {
-  const { text, handleClick } = useDadJoke();
-  
+  const { text, handleClick, fetchRandomDadJoke } = useDadJoke();
 
   return (
-<div>
-
+    <div>
+      <div className="SearchBar-container">
         <input
           type="text"
           id="search"
           placeholder="Search Jokes"
           className="searchBar"
-          
         />
-<div className="searchButton-container">
-        <button className="searchButton"></button>
-        <button className="searchButton"></button>
-        <button className="searchButton"></button>
-        <button className="searchButton"></button>
-      </div>
-    <div className="container">
-
-
-      <h1>Dad Joke #{text.id}</h1>
-      <p>{text.advice}</p>
-
-      <picture>
-        <source media="(min-width: 768px)" srcSet="" />
-        <img src="" alt="" />
-      </picture>
-
-      <div>
-        <button onClick={handleClick}>
-          <img src="./dice.png" alt="" />
+        <button id="searchSubmit" onClick={handleClick}>
+          Enter
         </button>
       </div>
-    </div>
+      <div className="searchButton-container">
+        <button className="searchButton"></button>
+        <button className="searchButton"></button>
+        <button className="searchButton"></button>
+        <button className="searchButton"></button>
+      </div>
+      <div className="container">
+        <h1>Dad Joke #{text.id}</h1>
+        <p>{text.advice}</p>
+        <picture>
+          <source media="(min-width: 768px)" srcSet="" />
+          <img src="" alt="" />
+        </picture>
+        <div>
+          <button className="tink" onClick={fetchRandomDadJoke}>
+            <img src="./dice.png" alt="" />
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
